9669e701c8bbbb8d27342cd4478f3b17c0c585dc
Deepak Gandhi <deepak.gandhi@genesys.com>
2026-04-07 16:26:05 +0530

    Initial Commit

---
+++ LAPS Password Retrieval/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LAPS Password Retrieval
+//
+//  Created by Deepak Gandhi on 07/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LAPS Password Retrieval/LAPS_Password_RetrievalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAPS_Password_RetrievalApp.swift
+//  LAPS Password Retrieval
+//
+//  Created by Deepak Gandhi on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LAPS_Password_RetrievalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
